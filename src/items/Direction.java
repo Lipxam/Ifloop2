461658f01d905261b05b5132cd343b03f814d871
Lipxam <lipxam@yahoo.com>
2014-05-08 12:26:00 -0400

    Added constructor

--- src/items/Direction.java
+++ src/items/Direction.java
@@ -2,10 +2,13 @@ package items;
 
 public class Direction
 {
-	int direction;
 	public static final int LEFT = 0;
 	public static final int RIGHT = 1;
 	public static final int UP = 2;
 	public static final int DOWN = 3;
-	
+	int direction;
+	public Direction(int d)
+	{
+		d = direction;
+	}
 }
