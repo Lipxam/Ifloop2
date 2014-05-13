d0c07d65737dabe62937dfab7f38a42217845700
Lipxam <lipxam@yahoo.com>
2014-05-13 12:46:14 -0400

    Update

--- src/machines/Kettle.java
+++ src/machines/Kettle.java
@@ -4,16 +4,49 @@ import items.Item;
 import items.Machine;
 import items.Materiel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Kettle extends Machine
 {
 	public Kettle()
 	{
-		inputs.add(Class.forName("Water"));
+		try
+		{
+			inputs.add((Class<? extends Materiel>)Class.forName("Electricity"));
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try
+		{
+			outputs.add((Class<? extends Materiel>)Class.forName("Steam"));
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
-	public List<Class<? extends Materiel>> step(List<Item> inputs)
+	public List<Materiel> step(List<Item> inputs)
 	{
-		return outputs;
+		List<Materiel> result = new ArrayList<Materiel>();
+		for(Class<? extends Materiel> o: outputs)
+		{
+			try
+			{
+				result.add(o.newInstance());
+			} catch (InstantiationException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 }
