b4ffc7ee7097bd1bc7ddca2fc03213e4ee674cfe
Jack <jack_alpert@ryecountryday.org>
2014-05-15 12:48:57 -0400

    made mahciens

---
+++ src/machines/Looper.java
@@ -0,0 +1,16 @@
+package machines;
+
+import java.util.List;
+import items.*;
+
+public class Looper extends Machine
+{
+
+	@Override
+	public List<Materiel> step(List<Item> inputs)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
