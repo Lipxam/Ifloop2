d9af77707464ee31ce890ccdc750f62e9a96dfd8
Lipxam <lipxam@yahoo.com>
2014-05-15 12:49:59 -0400

    gsafad

--- src/machines/LaserIn.java
+++ src/machines/LaserIn.java
@@ -1,6 +1,16 @@
 package machines;
+import items.Item;
+import items.Machine;
+import items.Materiel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LaserIn
 {
-
+	public LaserIn()
+	{
+		addInputType("Laser");
+	}
 }
