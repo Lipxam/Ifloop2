b047f7205c79fa3a2f71d03bc1289ea0d371259d
Lipxam <lipxam@yahoo.com>
2014-05-28 12:47:53 -0400

    sdada

--- src/graphics/GridDisplay.java
+++ src/graphics/GridDisplay.java
@@ -2,8 +2,17 @@ package graphics;
 
 import items.Machine;
 import items.Materiel;
+
+import java.awt.Color;
+import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
+import java.awt.Window;
+import java.awt.image.BufferStrategy;
+
 import machines.Wire;
 import data.Grid;
 
@@ -11,15 +20,64 @@ public class GridDisplay
 {
 	private Grid grid;
 	private Window window;
+	private Rectangle bounds;
 	
-	public GridDisplay(Grid g)
+	public GridDisplay(Grid g, Window w)
 	{
+		window = w;
 		grid = g;
+		
+		new Thread(new Runnable()
+		{
+			
+			public void run()
+			{
+				bounds = window.getBounds();
+				window.createBufferStrategy(2);
+				
+				BufferStrategy strat = window.getBufferStrategy();
+				Graphics g = strat.getDrawGraphics();
+				
+				
+				
+				try
+				{	
+					g.setColor(Color.BLUE);
+					g.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+					g.setColor(Color.BLACK);
+					for(int i = 0; i < 15; i++)
+					{
+						g.drawRect(i*50, 0, 10, bounds.height);
+					}
+					for(int z = 0; i < 15; i++)
+					{
+						g.drawRect(0, z*50, bounds.width, 10);
+					}
+					
+					
+					Thread.sleep(500);
+					
+						
+					}
+				
+				
+				catch(Exception ex)
+				{
+					ex.printStackTrace();
+					System.exit(0);
+				}
+			}).start();
+		}
+		
+		
 	}
 	
 	public void draw(Graphics g, Rectangle bounds)
 	{
 		// draw wires
+		
+		
+		
 		for(Machine i: grid.getMachines())
 		{
 			if(i instanceof Wire)
@@ -50,4 +108,6 @@ public class GridDisplay
 	{
 		this.grid = grid;
 	}
-}
+		
+		}
+		
